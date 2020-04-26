d140d8339862be8997cedf94b6fa060b0926658f
Emilia Gromulska <emgromulska@gmail.com>
2020-04-26 12:29:27 +0200

    5 day weather service

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,4 +1,5 @@
 public class Config {
-    public static final String APP_ID = "bc151f8d89fc8dbb1f49cde5f0030e10";
+    public static final String APP_ID = "f16898b1453340edd5123d717f02a81c";
     public static final String APP_URL = "http://api.openweathermap.org/data/2.5/weather";
+    public static final String APP_URL_5DAY = "http://api.openweathermap.org/data/2.5/forecast";
 }
--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -13,7 +13,7 @@ public class MainApp implements Runnable {
 
     private void startApp() {
         scanner = new Scanner(System.in);
-        System.out.println("Wybierz po czym chcesz znaleźć miejsce dla którego wyświetlisz pogodę \n0 - Zakończ działanie \n1 - Nazwa Miasta \n2 - Kod pocztowy");
+        System.out.println("\nWybierz, po czym chcesz znaleźć miejsce, dla którego wyświetlisz pogodę: \n0 - Zakończ działanie \n1 - Nazwa Miasta - pogoda aktualna \n2 - Kod pocztowy - pogoda aktualna \n3 - Nazwa miasta - pogoda na 5 dni \n4 - Kod pocztowy - pogoda na 5 dni");
         Integer name = scanner.nextInt();
         chooseTypeSearching(name);
     }
@@ -30,8 +30,17 @@ public class MainApp implements Runnable {
                 parseJson(connectByZipCode(getZipCode()));
                 startApp();
                 break;
+            case 3:
+                parseJson5DayWeather(connectByWeatherFor5Days(getCityName()));
+                startApp();
+                break;
+            case 4:
+                parseJson5DayWeather(connectByWeatherFor5DaysByZipCode(getZipCode()));
+                startApp();
+                break;
         }
     }
+
 
     public String getCityName() {
         System.out.println("Podaj nazwę miasta: ");
@@ -56,7 +65,8 @@ public class MainApp implements Runnable {
         String zipCode = scanner.next();
         return zipCode;
     }
-    public String connectByZipCode (String zipCode){
+
+    public String connectByZipCode(String zipCode) {
         String json = null;
         try {
             json = new HttpService().connect(Config.APP_URL + "?zip=" + zipCode + ",pl" + "&appid=" + Config.APP_ID);
@@ -64,8 +74,32 @@ public class MainApp implements Runnable {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return  json;
+        return json;
     }
+
+
+    public String connectByWeatherFor5Days(String cityName) {
+        String json = null;
+        try {
+            json = new HttpService().connect(Config.APP_URL_5DAY + "?q=" + cityName + "&appid=" + Config.APP_ID);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+
+    public String connectByWeatherFor5DaysByZipCode(String zipCode) {
+        String json = null;
+        try {
+            json = new HttpService().connect(Config.APP_URL_5DAY + "?zip=" + zipCode + ",pl" + "&appid=" + Config.APP_ID);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+
 
     private void parseJson(String json) {
         double temp;
@@ -90,6 +124,44 @@ public class MainApp implements Runnable {
             System.out.println("Zachmurzenie: " + clouds + "%");
             System.out.println("Ciśnienie: " + pressure + " hPa");
 
+        } else {
+            System.out.println("Error");
+        }
+    }
+
+    private void parseJson5DayWeather(String json) {
+        double temp;
+        int wind;
+        int pressure;
+        int clouds;
+        String dt_txt;
+
+        JSONObject rootObject = new JSONObject(json);
+        JSONArray weather5DayList = rootObject.getJSONArray("list");
+
+        if (rootObject.getInt("cod") == 200) {
+            JSONArray jsonArrayMain = rootObject.getJSONArray("list");
+            for (int i = 0; i < jsonArrayMain.length(); i++) {
+                JSONObject dayObject = weather5DayList.getJSONObject(i);
+                JSONObject mainObject = dayObject.getJSONObject("main");
+                DecimalFormat df = new DecimalFormat("#.##");
+                temp = mainObject.getDouble("temp");
+                temp = temp - 273;
+
+                JSONObject windObject = dayObject.getJSONObject("wind");
+                wind = windObject.getInt("speed");
+                pressure = mainObject.getInt("pressure");
+                JSONObject cloudsObject = dayObject.getJSONObject("clouds");
+                clouds = cloudsObject.getInt("all");
+                dt_txt = dayObject.getString("dt_txt");
+
+                System.out.println("\nData i godzina: " + dt_txt);
+                System.out.print("Temperatura: " + df.format(temp) + " \u00b0C");
+                System.out.print("  Wiatr: " + wind + " m/s");
+                System.out.print("  Zachmurzenie: " + clouds + "%");
+                System.out.print("  Ciśnienie: " + pressure + " hPa");
+
+            }
         } else {
             System.out.println("Error");
         }
