a33051acd29e415552820652c67cf35cd8c8122d
Emilia Gromulska <emgromulska@gmail.com>
2020-04-26 12:30:03 +0200

    5 day weather service

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,5 +1,5 @@
 public class Config {
-    public static final String APP_ID = "f16898b1453340edd5123d717f02a81c";
+    public static final String APP_ID = "your_id";
     public static final String APP_URL = "http://api.openweathermap.org/data/2.5/weather";
     public static final String APP_URL_5DAY = "http://api.openweathermap.org/data/2.5/forecast";
 }
